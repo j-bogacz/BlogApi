402abd7964be1dc6d00eaa0edcb90578a04a152d
Jakub Bogacz <j.bogacz@outlook.com>
2016-09-24 18:02:56 +0200

    Basic authenticator added (no real implementation yet)

---
+++ api/modules/authenticator.server.module.js
@@ -0,0 +1,25 @@
+var authenticator = function () {
+
+    // dummy token validation flag
+    var isTokenValid = true;
+
+    // checks if user is authenticated
+    var isUserAuthenticated = function (req, res, next) {
+
+        // do any checks you want to in here, e.g. verify token
+        if (!isTokenValid) {
+            return res.status(401).send({
+                message: 'User is not authenticated',
+                details: 'Failed to authenticate token'
+            });
+        }
+
+        next();
+    }
+
+    return {
+        'isUserAuthenticated': isUserAuthenticated
+    };
+} ();
+
+module.exports = authenticator;  
